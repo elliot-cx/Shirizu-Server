6a5342bba67a449d316a4c60bf54321384e9783f
elliot-cx <derfire22@gmail.com>
2023-10-29 15:51:18 +0100

    update model

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,7 +2,7 @@ import { model, Schema } from "mongoose";
 
 const userSchema = new Schema({
     name: String,
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     hashedPassword: { type: String, required: true },
     createdAt: String
 });
