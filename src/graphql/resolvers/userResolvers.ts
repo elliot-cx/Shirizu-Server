2f723c6f3067cdd756f750752ca564a19f5fc826
elliot-cx <derfire22@gmail.com>
2023-10-29 15:54:41 +0100

    update idents

--- src/graphql/resolvers/userResolvers.ts
+++ src/graphql/resolvers/userResolvers.ts
@@ -4,74 +4,74 @@ import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 
 const userResolvers = {
-    Query: {
-        getUsers: async (_, { }) => {
-            return await user.find();
-        },
-    },
-    Mutation: {
-        registerUser: async (_, { input: { name, email, password } }) => {
-            // Find existing acccount
-            const oldUser = await user.findOne({ email: email });
+   Query: {
+      getUsers: async (_, { }) => {
+         return await user.find();
+      },
+   },
+   Mutation: {
+      registerUser: async (_, { input: { name, email, password } }) => {
+         // Find existing acccount
+         const oldUser = await user.findOne({ email: email });
 
-            if (oldUser) {
-                throw new GraphQLError(`A user is already registered with the email '${email}'`, {
-                    extensions: {
-                        code: 'USER_ALREADY_EXIST'
-                    }
-                });
-            }
+         if (oldUser) {
+            throw new GraphQLError(`A user is already registered with the email '${email}'`, {
+               extensions: {
+                  code: 'USER_ALREADY_EXIST'
+               }
+            });
+         }
 
-            // Adding user to the database
-            const res = await new user({
-                name: name,
-                email: email,
-                hashedPassword: bcrypt.hashSync(password),
-                createdAt: new Date().toISOString()
-            }).save();
+         // Adding user to the database
+         const res = await new user({
+            name: name,
+            email: email,
+            hashedPassword: bcrypt.hashSync(password),
+            createdAt: new Date().toISOString()
+         }).save();
 
-            // Return user created ID
-            return res._id;
-        },
-        loginUser: async (_, { input: { email, password } }) => {
-            // Search user
-            const foundUser = await user.findOne({ email });
-            if (!foundUser) {
-                throw new GraphQLError(`User not found`, {
-                    extensions: {
-                        code: 'USER_NOT_FOUND'
-                    }
-                });
-            }
+         // Return user created ID
+         return res._id;
+      },
+      loginUser: async (_, { input: { email, password } }) => {
+         // Search user
+         const foundUser = await user.findOne({ email });
+         if (!foundUser) {
+            throw new GraphQLError(`User not found`, {
+               extensions: {
+                  code: 'USER_NOT_FOUND'
+               }
+            });
+         }
 
-            // Check password
-            const validPassword = await bcrypt.compare(password, foundUser.hashedPassword);
-            if (!validPassword) {
-                throw new GraphQLError(`Invalid password`, {
-                    extensions: {
-                        code: 'INVALID_PASSWORD'
-                    }
-                });
-            }
+         // Check password
+         const validPassword = await bcrypt.compare(password, foundUser.hashedPassword);
+         if (!validPassword) {
+            throw new GraphQLError(`Invalid password`, {
+               extensions: {
+                  code: 'INVALID_PASSWORD'
+               }
+            });
+         }
 
-            // Generating the access token
-            const accessToken = jwt.sign(
-                { sub: foundUser._id },
-                process.env.SECRET_KEY,
-                { expiresIn: '1h' }
-            );
+         // Generating the access token
+         const accessToken = jwt.sign(
+            { sub: foundUser._id },
+            process.env.SECRET_KEY,
+            { expiresIn: '1h' }
+         );
 
-            // Generating the refresh token
-            const refreshToken = jwt.sign(
-                { sub: foundUser._id },
-                process.env.SECRET_KEY,
-                { expiresIn: '7d' }
-            );
+         // Generating the refresh token
+         const refreshToken = jwt.sign(
+            { sub: foundUser._id },
+            process.env.SECRET_KEY,
+            { expiresIn: '7d' }
+         );
 
-            // Return tokens
-            return { accessToken, refreshToken };
-        }
-    }
+         // Return tokens
+         return { accessToken, refreshToken };
+      }
+   }
 };
 
 export default userResolvers;
--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,26 @@ import userTypeDefs from './graphql/typedefs/userTypeDefs';
 import userResolvers from './graphql/resolvers/userResolvers';
 
 const server = new ApolloServer({
-    typeDefs: [userTypeDefs],
-    resolvers: [userResolvers],
-    formatError: (error) => {
-        const { extensions: { code, ...restExtensions }, message } = error;
-        return {
-          message,
-          extensions: {
+   typeDefs: [userTypeDefs],
+   resolvers: [userResolvers],
+   formatError: (error) => {
+      const { extensions: { code, ...restExtensions }, message } = error;
+      return {
+         message,
+         extensions: {
             code,
-          },
-        };
-      },
+         },
+      };
+   },
 });
 
 mongoose.connect(process.env.MONGODB)
-    .then(() => {
-        console.log(chalk.green("[✓]"), "MongoDB connection successful");
-        return startStandaloneServer(server, {
-            listen: { port: parseInt(process.env.APOLLO_PORT) }
-        });
-    })
-    .then(({url}) => {
-        console.log(`🚀 Apollo server running at : ${url}`);
-    })
+   .then(() => {
+      console.log(chalk.green("[✓]"), "MongoDB connection successful");
+      return startStandaloneServer(server, {
+         listen: { port: parseInt(process.env.APOLLO_PORT) }
+      });
+   })
+   .then(({ url }) => {
+      console.log(`🚀 Apollo server running at : ${url}`);
+   })
